06c954eebf50bcdc4ac0d1de1673ea4a4bbe7e9b
Peter Grimm <pagrimm@gmail.com>
2020-10-04 18:57:40 -0700

    add viewmodels and launchsettings

---
+++ backlogger/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Backlogger.ViewModels
+{
+  public class RegisterViewModel
+  {
+    [Required]
+    [UserName]
+    [Display(Name = "UserName")]
+    public string UserName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+  }
+}
