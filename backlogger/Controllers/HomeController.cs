20fcf320efa9d4d74830c02c82e020bc3c53c810
Peter Grimm <pagrimm@gmail.com>
2020-10-05 19:23:29 -0700

    add template for bootstrap nav and sidebar, add bootstrap and jquery js files

---
+++ backlogger/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Backlogger.Models;
+using Backlogger.ViewModels;
+
+namespace Backlogger.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
